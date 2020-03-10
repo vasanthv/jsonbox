9c12b2604a89d10638445fbb0fe8a1ec4dcba68f
Vasanth.V <imvasanthv@gmail.com>
2020-03-10 13:01:23 +0530

    Added v2

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-const bodyParser = require("body-parser");
-const express = require("express");
-const path = require("path");
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
 const cors = require('cors');
-const config = require("./src/config");
-const routes = require("./src/routes");
+const config = require('./src/config');
+const routes = require('./src/routes');
 
 const app = express();
 
-app.enable("trust proxy");
+app.enable('trust proxy');
 // set express server middlewares
 app.use(cors());
-app.use(express.static(path.join(__dirname, "www")));
+app.use(express.static(path.join(__dirname, 'www')));
 app.use(bodyParser.json());
+
+app.get('/v2', (req, res) => res.sendFile(path.join(__dirname, 'www/index.html')));
 
 app.use(routes);
 
 app.listen(config.PORT, err => {
 	if (err) console.error(err);
-	console.log("Server started on " + config.PORT);
+	console.log('Server started on ' + config.PORT);
 });
