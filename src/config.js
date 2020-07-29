1cc5e38c116a610e005ff9c4e150431e4a9a0898
Vasanth.V <imvasanthv@gmail.com>
2020-07-29 22:38:12 +0530

    Cofigurize requests per hour

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ module.exports = {
   SIZE_LIMIT: 50, // mentioned in KB
   PORT: process.env.PORT || 3000,
   MONGO_URL: process.env.MONGODB_URI || "mongodb://localhost:27017/jsonbox-io-dev",
+  REQUEST_LIMIT_PER_HOUR: 100,
 };
