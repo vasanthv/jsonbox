ad74f396d4d672b4d33539a7312af2b251abc28e
Aakash Goel <aakash.lpin@gmail.com>
2020-08-16 23:41:19 +0530

    updated limit

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  SIZE_LIMIT: 1000, // mentioned in KB
+  SIZE_LIMIT: 10000, // mentioned in KB
   PORT: process.env.PORT || 3000,
   MONGO_URL: process.env.MONGODB_URI || "mongodb://localhost:27017/jsonbox-io-dev",
   REQUEST_LIMIT_PER_HOUR: 99999,
