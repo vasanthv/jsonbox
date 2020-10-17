aa68c042a1e73f2a0aa3db9d318ca0b9df0ba58b
Ron Harlev <harlev@gmail.com>
2020-10-17 00:03:55 -0700

    return 403 { message: "Forbidden" } for IP filter fail

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const config = require('./src/config');
 const routes = require('./src/routes');
 const ipfilter = require('express-ipfilter').IpFilter
+const IpDeniedError = require('express-ipfilter').IpDeniedError
 
 
 const app = express();
@@ -25,6 +26,13 @@ app.use(bodyParser.json());
 app.get('/v2', (req, res) => res.sendFile(path.join(__dirname, 'www/index.html')));
 
 app.use(routes);
+
+app.use((err, req, res, next) => {
+    console.error(err)
+    if (err instanceof IpDeniedError) {
+    	res.status(403).json({ message: "Forbidden" });
+	}
+});
 
 app.listen(config.PORT, err => {
 	if (err) console.error(err);
