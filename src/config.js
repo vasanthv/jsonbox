2751626c967924ee8a64a49880b5992d92afeb5b
Vasanth.V <imvasanthv@gmail.com>
2021-05-31 07:43:18 +0530

    Disabled the data expiry behaviour as default

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ module.exports = {
 	PORT: process.env.PORT || 3000,
 	MONGO_URL: process.env.MONGODB_URI || "mongodb://localhost:27017/jsonbox-io-dev",
 	REQUEST_LIMIT_PER_HOUR: 100,
-	ENABLE_DATA_EXPIRY: true, // Once switched on the index will be be set in mongodb. Might need to remove it in order to switch off the behaviour
+	ENABLE_DATA_EXPIRY: false, // Once switched on the index will be be set in mongodb. Might need to remove it in order to switch off the behaviour
 	DATA_EXPIRY_IN_DAYS: 30,
-	FILTER_IP_SET: [],  // example ['172.29.0.1']
-	FILTER_OPTIONS: { mode: 'allow' },
+	FILTER_IP_SET: [], // example ['172.29.0.1']
+	FILTER_OPTIONS: { mode: "allow" },
 };
